ef20cb5d60a01e4f368915b9b3a50f970d458c38
Oriol VidalCortes <ovidalcortes@gmail.com>
2019-09-07 15:14:07 +0200

    init eslint

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -3,4 +3,4 @@
 import React from 'react';
 import '../styles/styles.css';
 
-export default () => <div className="app">The manager</div>
+export default () => <div className="app">The manager</div>;
